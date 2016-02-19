47ecac9a2adccc06ab6f97dcd3847c883861111f
Pavel <pavel.cherednichenko.ua@gmail.com>
2016-02-19 12:29:23 +0200

    Delete global.h

--- global.h
+++
@@ -1,9 +0,0 @@
-#ifndef Plane_config_h
-#define Plane_config_h
-
-#define PLAYER_LIMIT_LEFT -122
-#define PLAYER_LIMIT_RIGHT 130
-#define PLAYER_LIMIT_TOP 500
-#define PLAYER_LIMIT_DOWN -50
-
-#endif
